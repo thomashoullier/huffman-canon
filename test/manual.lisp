7b49cca1322515ee144c5da62df521f76535f2a5
Thomas HOULLIER (Sophia Engineering) <thomas.houllier@sophiaengineering.com>
2019-12-22 20:45:13 +0100

    Added manual tests.

---
+++ test/manual.lisp
@@ -0,0 +1,9 @@
+;;;; Manual tests for huffman-canon.
+
+;;; RFC1951 example
+(defparameter *lengths* #(2 3 3 3 3 3 4 4))
+(defparameter *hfm*
+  (huffman-canon:make-huffman-canon-from-code-lengths *lengths*))
+
+(format t "~&~A~%" (huffman-canon:encode *hfm* #(0 1 2 3)))
+(format t "~&~A~%" (huffman-canon:decode *hfm* #*00010011100))
